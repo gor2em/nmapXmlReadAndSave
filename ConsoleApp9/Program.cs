4f440696f46e8df44cd75253bddd159855e9d2f8
gor2em <grkem.squash99@gmail.com>
2020-04-04 01:01:35 +0300

    nmap xml read and save

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            string dosya = @"out.xml";
+            xmldoc.Load(dosya);
+            xmldoc.Save(Console.Out);
+            xmldoc.Save("test.xml");
+            Console.ReadKey();
+        }
+    }
+}
